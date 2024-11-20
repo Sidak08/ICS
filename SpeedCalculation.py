1049589ac4389113df3c1ea0bd9b80b8d4c57565
Sidak08 <magicalsidak@gmail.com>
2024-11-20 09:13:21 -0500

    comp sci class Nov 20 2024

--- SpeedCalculation.py
+++ SpeedCalculation.py
@@ -27,11 +27,11 @@ speed = [0.0] * 3
 
 for i in range(len(dist)):
     #promt user to input the distance
-    dist[i] = float(input("Enter the distance traveled in meter: "))
+    dist[i] = float(input(f"Enter the distance {i+1} traveled in meter: "))
 
     #promt and get time from user
-    time[i] = float(input("enter the time in seconds: "))
+    time[i] = float(input(f"enter the time {i+1} in seconds: "))
 
     #calc the speed and display it
     speed[i] = calcSpeed(dist[i], time[i])
-    print(f"The average speed is {speed[i]} m/s")
+    print(f"The average speed {i+1} is {speed[i]} m/s")
