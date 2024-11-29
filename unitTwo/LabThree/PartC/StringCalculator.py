ab0b6f9f8642397f7b1ad37440eb3d4c2f99791b
Sidak08 <magicalsidak@gmail.com>
2024-11-29 10:32:42 -0500

    finished the file structure

---
+++ unitTwo/LabThree/PartC/StringCalculator.py
@@ -0,0 +1,8 @@
+#Sidak
+def calc(str):
+    return eval(str.replace("x", "*"))
+
+# print(calc("0003.00+0004.56"))
+print(calc("3256.40+4765.56"))
+print(3256.40+4765.56)
+# print(calc("3256.40x4765.56"))
