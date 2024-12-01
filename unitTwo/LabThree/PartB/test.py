6284dc11cd8931cdf0606cc0fac7b26fd66d7ebb
Dhene123 <Dhenushanab@gmail.com>
2024-12-01 09:33:54 -0500

    dumb

--- unitTwo/LabThree/PartB/test.py
+++
@@ -1,3 +0,0 @@
-street = "Daviselm St." 
-address = "45"
-print (chr(ord(street[0])))
--- unitTwo/LabThree/PartC/ExpressionLoad.py
+++ unitTwo/LabThree/PartC/ExpressionLoad.py
@@ -1,1 +1,32 @@
 #Denu
+'''
+File: Loader.py
+Author: Dhenushan Ramesh
+Date: Nov 30, 2024
+
+Description:
+This program recives a file name from the User Interface program and then opens
+the specified text file, reads it and then send the info contained in it back to
+the user Interface Program
+
+Function list:
+
+load_file(filename)
+    - function to open a txt file and load the info into a list
+    - parameters needed: filename(has to be a string)
+    - return: list with string data from the txt file
+
+'''
+#================ Functions ===================
+
+def load_file(filename):
+    fileR = open(filename,"r")
+    size = fileR.readline()
+    size = int(size.strip())
+    file_info = [None]*size
+    for i in range(size):
+        file_info[i] = fileR.readline().strip() 
+    fileR.close()
+    return file_info
+
+#================= Main Program ==================
