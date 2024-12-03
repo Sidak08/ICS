691df3fd7f291e8de9b1b64eb4fbb7419a091476
Sidak08 <magicalsidak@gmail.com>
2024-12-03 08:57:10 -0500

    done calc

--- unitTwo/LabThree/PartC/StringCalculator.py
+++ unitTwo/LabThree/PartC/StringCalculator.py
@@ -1,8 +1,38 @@
-#Sidak
-def calc(str):
-    return eval(str.replace("x", "*"))
+from ast import Num
 
-# print(calc("0003.00+0004.56"))
-print(calc("3256.40+4765.56"))
-print(3256.40+4765.56)
-# print(calc("3256.40x4765.56"))
+
+def calc(str: str) -> str:
+    for i in range(1, len(str)):
+        str = str.strip()
+        match (str[i]):
+            case "+":
+                num1 = float(str[:i])
+                num2 = float(str[i+1:])
+                opp = float(str[i+1:])
+                opp = str[i]
+                answer = num1 + num2
+                break
+            case "-":
+                num1 = float(str[:i])
+                num2 = float(str[i+1:])
+                opp = str[i]
+                answer = num1 - num2
+            case "x":
+                num1 = float(str[:i])
+                num2 = float(str[i+1:])
+                opp = str[i]
+                answer = num1 * num2
+            case "/":
+                num1 = float(str[:i])
+                num2 = float(str[i+1:])
+                opp = str[i]
+                answer = num1 / num2
+            case "%":
+                num1 = float(str[:i])
+                num2 = float(str[i+1:])
+                opp = str[i]
+                answer = num1 / num2
+
+    return "{:,.2f} {} {:,.2f} = {:,.2f}".format(num1, opp, num2, answer)
+
+print(calc("-003.00+0004.56"))
