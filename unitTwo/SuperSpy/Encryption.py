0b1ffef55409fc264dc6e54d04de25def856be75
Sidak08 <magicalsidak@gmail.com>
2024-12-09 09:05:19 -0500

    basic

---
+++ unitTwo/SuperSpy/Encryption.py
@@ -0,0 +1,15 @@
+#Sidak
+
+
+listCapitalLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+listLowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', '
+
+def is_not_a_letter(character: str) -> bool:
+    print("returns falseif the character is a letter.")
+
+def encode(letter:str, key: int)-> str:
+    print("returns an encrypted character")
+
+def decode (letter: str, key: int)-> str
+    print("returns a decrypted character")
