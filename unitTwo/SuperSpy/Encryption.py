c07d9852063119754cc7e48d1127829fce9e4f76
Sidak08 <magicalsidak@gmail.com>
2024-12-10 13:31:42 -0500

    done encption

--- unitTwo/SuperSpy/Encryption.py
+++ unitTwo/SuperSpy/Encryption.py
@@ -1,15 +1,53 @@
 #Sidak
 
-
-listCapitalLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-listLowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z']
+capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 
 def is_not_a_letter(character: str) -> bool:
-    print("returns falseif the character is a letter.")
+    if capitalLetters.find(character) != -1 or lowercaseLetters.find(character) != -1:
+        return True
+    return False
 
 def encode(letter:str, key: int)-> str:
-    print("returns an encrypted character")
+    global capitalLetters, lowercaseLetters
+    if (is_not_a_letter == False):
+        if capitalLetters.find(letter) != -1:
+            index = capitalLetters.find(letter)
+            newIndex = (index + key) % 26
+            return capitalLetters[newIndex]
+        if lowercaseLetters.find(letter) != -1:
+            index = lowercaseLetters.find(letter)
+            newIndex = (index + key) % 26
+            return lowercaseLetters[newIndex]
+    return letter
 
 def decode (letter: str, key: int)-> str:
-    print("returns a decrypted character")
+    global capitalLetters, lowercaseLetters
+    if (is_not_a_letter == False):
+        if capitalLetters.find(letter) != -1:
+            index = capitalLetters.find(letter)
+            newIndex = (index - key) % 26
+            return capitalLetters[newIndex]
+        if lowercaseLetters.find(letter) != -1:
+            index = lowercaseLetters.find(letter)
+            newIndex = (index - key) % 26
+            return lowercaseLetters[newIndex]
+    return letter
+
+def encoder(sentence: str, key: int) -> str:
+    answer = ""
+    for i in range(len(sentence)):
+        answer += encode(sentence[i], key)
+    return answer
+
+def decoder(sentence: str, key: int) -> str:
+    answer = ""
+    for i in range(len(sentence)):
+        answer += decode(sentence[i], key)
+    return answer
+
+if __name__ == "__main__":
+    print(encode("!", 27))
+    print(decode("D", 3))
+    print(encoder("sidak", 3))
+    print(decoder(encoder("sidak", 3), 3))
