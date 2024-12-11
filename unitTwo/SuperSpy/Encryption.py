0b589e7bc9bf8746a84c4a7727b8612689d53831
Sidak08 <magicalsidak@gmail.com>
2024-12-11 09:02:25 -0500

    encrypter works

--- unitTwo/SuperSpy/Encryption.py
+++ unitTwo/SuperSpy/Encryption.py
@@ -1,37 +1,40 @@
 #Sidak
 
-capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+CAPITAL_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+
+def genEncryptionKey(sentence: str) -> int:
+    return len(sentence)
 
 def is_not_a_letter(character: str) -> bool:
-    if capitalLetters.find(character) != -1 or lowercaseLetters.find(character) != -1:
-        return True
-    return False
+    if CAPITAL_LETTERS.find(character) != -1 or LOWERCASE_LETTERS.find(character) != -1:
+        return False
+    return True
 
 def encode(letter:str, key: int)-> str:
-    global capitalLetters, lowercaseLetters
-    if (is_not_a_letter == False):
-        if capitalLetters.find(letter) != -1:
-            index = capitalLetters.find(letter)
+    global CAPITAL_LETTERS, LOWERCASE_LETTERS
+    if (is_not_a_letter(letter) == False):
+        if CAPITAL_LETTERS.find(letter) != -1:
+            index = CAPITAL_LETTERS.find(letter)
             newIndex = (index + key) % 26
-            return capitalLetters[newIndex]
-        if lowercaseLetters.find(letter) != -1:
-            index = lowercaseLetters.find(letter)
+            return CAPITAL_LETTERS[newIndex]
+        if LOWERCASE_LETTERS.find(letter) != -1:
+            index = LOWERCASE_LETTERS.find(letter)
             newIndex = (index + key) % 26
-            return lowercaseLetters[newIndex]
+            return LOWERCASE_LETTERS[newIndex]
     return letter
 
 def decode (letter: str, key: int)-> str:
-    global capitalLetters, lowercaseLetters
-    if (is_not_a_letter == False):
-        if capitalLetters.find(letter) != -1:
-            index = capitalLetters.find(letter)
+    global CAPITAL_LETTERS, LOWERCASE_LETTERS
+    if (is_not_a_letter(letter) == False):
+        if CAPITAL_LETTERS.find(letter) != -1:
+            index = CAPITAL_LETTERS.find(letter)
             newIndex = (index - key) % 26
-            return capitalLetters[newIndex]
-        if lowercaseLetters.find(letter) != -1:
-            index = lowercaseLetters.find(letter)
+            return CAPITAL_LETTERS[newIndex]
+        if LOWERCASE_LETTERS.find(letter) != -1:
+            index = LOWERCASE_LETTERS.find(letter)
             newIndex = (index - key) % 26
-            return lowercaseLetters[newIndex]
+            return LOWERCASE_LETTERS[newIndex]
     return letter
 
 def encoder(sentence: str, key: int) -> str:
@@ -47,7 +50,8 @@ def decoder(sentence: str, key: int) -> str:
     return answer
 
 if __name__ == "__main__":
-    print(encode("!", 27))
-    print(decode("D", 3))
-    print(encoder("sidak", 3))
-    print(decoder(encoder("sidak", 3), 3))
+    print(encode("D", -27))
+    print(encode("d", -2))
+    # print(decode("D", -1))
+    # print(encoder("sidak", 3))
+    # print(decoder(encoder("sidak", 3), 3))
