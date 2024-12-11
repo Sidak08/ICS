80b73e79936afcfe95301faa3520f88e7f2836c9
Dhene123 <Dhenushanab@gmail.com>
2024-12-10 20:11:22 -0500

    done something

--- unitTwo/SuperSpy/GUI.py
+++ unitTwo/SuperSpy/GUI.py
@@ -6,37 +6,53 @@ Date: Nov 30, 2024
 
 '''
 
-import tkinter as tk
 
-root = tk.Tk()
-root.title("File Name Input")
+root = Tk()
+root.title("Super Spy Program")
+root.configure(bg="black")
 
-FileN = tk.Label(root, text="Enter File Name:", width=20, height=2)
-FileN.grid(row=0, column=0, padx=10, pady=10)
+Title = Label(root, text="Encrypt Message", width=20, height=1, bg="black", fg="white")
+Title.grid(row=0, column=0, padx=10, pady=10)
+Title.config(font=(20))
 
-entry = tk.Entry(root, width=40)
-entry.grid(row=0, column=1, padx=10, pady=10)
+FileN = Label(root, text="Enter File Name:", width=20, height=2, bg="black", fg="white")
+FileN.grid(row=1, column=0, padx=10, pady=10)
 
-FileO = tk.Label(root, text="Enter Output File name:", width=20, height=2)
-FileO.grid(row=1, column=0, padx=10, pady=10)
+entry = Entry(root, width=40, bg="black", fg="white")
+entry.grid(row=1, column=1, padx=10, pady=10)
 
-entry2 = tk.Entry(root, width=40)
-entry2.grid(row=1, column=1, padx=10, pady=10)
+FileO = Label(root, text="Enter Output File name:", width=20, height=2, bg="black", fg="white")
+FileO.grid(row=2, column=0, padx=10, pady=10)
 
-EncryptionK = tk.Label(root, text="Enter Encryption Key:", width=20, height=2)
-EncryptionK.grid(row=2, column=0, padx=10, pady=10)
+entry2 = Entry(root, width=40, bg="black", fg="white")
+entry2.grid(row=2, column=1, padx=10, pady=10)
 
-entry3 = tk.Entry(root, width=40)
-entry3.grid(row=2, column=1, padx=10, pady=10)
+EncryptionK = Label(root, text="Enter Encryption Key:", width=20, height=2, bg="black", fg="white")
+EncryptionK.grid(row=3, column=0, padx=10, pady=10)
 
-orblock = tk.Label(root, text="OR", width=20, height=2)
-orblock.grid(row=1, column=2, padx=10, pady=10)
+entry3 = Entry(root, width=40, bg="black", fg="white")
+entry3.grid(row=3, column=1, padx=10, pady=10)
 
-FileN = tk.Label(root, text="Enter File Name:", width=20, height=2)
-FileN.grid(row=0, column=3, padx=10, pady=10)
+orblock = Label(root, text="OR", width=20, height=2, bg="black", fg="white")
+orblock.grid(row=2, column=2, padx=10, pady=10)
 
-entry = tk.Entry(root, width=40)
-entry.grid(row=0, column=4, padx=10, pady=10)
+message = Label(root, text="Enter message:", width=20, height=2, bg="black", fg="white")
+message.grid(row=1, column=3, padx=10, pady=10)
+
+entry4 = Entry(root, width=40, bg="black", fg="white")
+entry4.grid(row=1, column=4, padx=10, pady=10)
+
+EncryptKey = Label(root, text="Encrypted Output:", width=20, height=2, bg="black", fg="white")
+EncryptKey.grid(row=2, column=3, padx=10, pady=10)
+
+entry5 = Entry(root, width=40, bg="black", fg="white")
+entry5.grid(row=2, column=4, padx=10, pady=10)
+
+EncryptionK = Label(root, text="Enter Encryption Key:", width=20, height=2, bg="black", fg="white")
+EncryptionK.grid(row=3, column=3, padx=10, pady=10)
+
+entry6 = Entry(root, width=40, bg="black", fg="white")
+entry6.grid(row=3, column=4, padx=10, pady=10)
 
 # Run the application
 root.mainloop()
